391378c1f6e2e7427c8f56868fda5c2f2791df05
Zhile Lin <zhlin04@gmail.com>
2022-04-25 15:36:03 -0700

    frist part finished

--- W2D1/bootcamp_project/lib/bootcamp.rb
+++ W2D1/bootcamp_project/lib/bootcamp.rb
@@ -7,6 +7,43 @@ class Bootcamp
         @teachers = []
         @grades = Hash.new{|k,v| k[v] = []}
     end
-
+     
+    def name 
+        @name
+    end
     
+    def slogan
+        @slogan
+    end
+
+    def teachers
+        @teachers
+    end
+
+    def students
+        @students
+    end
+
+    def hire(teacher)
+        @teachers.push(teacher)
+    end
+
+    def enroll(student)
+
+        if @students.length  < @student_capacity
+             @students << student
+             true
+        else
+            false
+
+        end
+       
+    end
+
+   def enrolled?(student)
+        return true if @students.include?(student)
+        false
+   end
+
+
 end
