1ae863697ba0c8ad5aba1081a7b3a9cfa0980ed2
Zhile Lin <zhlin04@gmail.com>
2022-04-25 15:21:28 -0700

    initialize

--- W2D1/bootcamp_project/lib/bootcamp.rb
+++ W2D1/bootcamp_project/lib/bootcamp.rb
@@ -1,3 +1,12 @@
 class Bootcamp
-  
+    def initialize(name,slogan,student_capacity)
+        @name = name
+        @slogan = slogan
+        @student_capacity = student_capacity
+        @students = []
+        @teachers = []
+        @grades = Hash.new{|k,v| k[v] = []}
+    end
+
+    
 end
