474c8fd4c1edf225b622050cc9ead617b6fd73ce
Zhile Lin <zhlin04@gmail.com>
2022-04-25 15:10:51 -0700

    finshed dog project

--- W2D1/dog_project/lib/dog.rb
+++ W2D1/dog_project/lib/dog.rb
@@ -18,4 +18,25 @@ class Dog
    def age
       @age
    end
+
+   def age= number
+      @age = number
+   end
+   
+   def bark
+      if @age > 3
+         return @bark.upcase
+      else
+         return @bark.downcase
+      end
+   end
+   
+  def favorite_foods
+      @favorite_foods
+  end
+    
+  def favorite_food?(food)
+       @favorite_foods.map{|fav| fave = fav.downcase}.include?(food.downcase)
+  end
+
 end
