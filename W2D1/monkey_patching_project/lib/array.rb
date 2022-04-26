355ae87528ac9e2e55d293cc3531e6855a1e2a10
Zhile Lin <zhlin04@gmail.com>
2022-04-25 17:20:15 -0700

    finished Part 2

--- W2D1/monkey_patching_project/lib/array.rb
+++ W2D1/monkey_patching_project/lib/array.rb
@@ -63,18 +63,51 @@ end
 #*****************************************************************
 
 # Part 2
+#my_count
+def my_count(value)
+    count = 0
+    self.each {|el| count += 1 if value == el}
+    count
+end
+
+#my_index
+def my_index(value)
+  
+  self.each_index {|i| return i  if self[i] == value} 
+
+  nil
+
+end
 
 
+#my_uniq
+def my_uniq
+    
+newArr = []
+    self.each do |el|
+        newArr << el unless newArr.include?(el)
+    end
+
+newArr
+
+end
 
 
+#my_transpose
 
+def my_transpose
+    transposeArr = []
+   (0...self[0].length).each do |i|
+          newArr = []
+        (0...self.length).each do |j|
+               newArr << self[j][i]
+        end
+         temp = newArr
+         transposeArr << temp
+    end
 
+  transposeArr
+end
 
-
-
-
-
-  end
-
-
+end
 
